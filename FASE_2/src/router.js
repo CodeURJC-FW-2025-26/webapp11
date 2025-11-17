2d6c8dd17dcf3c62c9dbfa24681c6ddb0e50077a
blodresg <adrivarfer03@gmail.com>
2025-11-17 23:54:02 +0100

    Links fixed

--- FASE_2/src/router.js
+++ FASE_2/src/router.js
@@ -69,27 +69,27 @@ router.post('/brand/new', upload.single('logo'), async (req, res) => {
 
     //Empty fields
     if (!req.body.brandName || !req.body.country || !req.body.description) {
-        return res.status(400).render('error', { message: "Some fields are missing" ,link : "/new_brand.html"});
+        return res.status(400).render('error', { message: "Some fields are missing" ,link : "/brand/new",page:"New Brand"});
     }
     if (!req.file) {
-        return res.status(400).render('error', { message: "You must upload a brand logo",link : "/new_brand.html" });
+        return res.status(400).render('error', { message: "You must upload a brand logo",link : "/brand/new",page:"New Brand" });
     }
     //Valid brand 
     if (!/^[A-ZÁÉÍÓÚÑ][a-zA-Z0-9\sáéíóúñÁÉÍÓÚÑ]{0,29}$/.test(req.body.brandName)) {
-        return res.status(400).render('error', { message: "Brand name must start with an uppercase letter and have a maximum of 30 characters",link : "/brand/new" });
+        return res.status(400).render('error', { message: "Brand name must start with an uppercase letter and have a maximum of 30 characters",link : "/brand/new",page:"New Brand" });
 }
     //Checks if the brand name already exists
     const existingBrand = await catalog.getBrandByName(req.body.brandName);
     if (existingBrand) {
-        return res.status(400).render('error', { message: "Brand name already exists",link : "/new_brand.html"});
+        return res.status(400).render('error', { message: "Brand name already exists",link : "/brand/new",page:"New Brand"});
     }
     //Description lenght
     if (req.body.description.length < 10 || req.body.description.length > 300) {
-        return res.status(400).render('error', { message: "Description must be between 10 and 300 characters",link : "/new_brand.html"});
+        return res.status(400).render('error', { message: "Description must be between 10 and 300 characters",link : "/brand/new",page:"New Brand"});
     }
     //Country
     if (!/^[A-Za-zÁÉÍÓÚÑáéíóúñ\s]{2,60}$/.test(req.body.country)) {
-        return res.status(400).render('error', { message: "Country must contain only letters and must be between 2 and 60 characters",link : "/new_brand.html"});
+        return res.status(400).render('error', { message: "Country must contain only letters and must be between 2 and 60 characters",link : "/brand/new",page:"New Brand"});
     }
 
     let brandEntity = {
